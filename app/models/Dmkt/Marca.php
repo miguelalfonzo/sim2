df6a3096d2b533307dfbdf213839fd744c293c57
cesar <cesarhm1687@gmail.com>
2014-08-14 17:10:07 -0500

    make model marca and solicitudefamily

---
+++ app/models/Dmkt/Marca.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Developer
+ * Date: 13/08/14
+ * Time: 12:13 PM
+ */
+namespace Dmkt;
+
+class Marca extends \Eloquent {
+
+    public $timestamps = false;
+
+
+
+
+}
---
+++ app/models/Dmkt/SolicitudeFamily.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Developer
+ * Date: 13/08/14
+ * Time: 10:43 AM
+ */
+
+namespace Dmkt;
+use \Eloquent;
+
+class SolicitudeFamily extends Eloquent{
+
+
+    protected $table = 'DMKT_RG_SOLICITUD_FAMILIA';
+    protected $primaryKey = 'IDSOLICITUD_FAMILIA';
+    public $timestamps = false;
+
+    function searchId(){
+        $lastId = SolicitudeFamily::orderBy('idsolicitud_familia', 'DESC')->first();
+        if($lastId == null){
+            return 0;
+        }else{
+            return $lastId->idsolicitud_familia;
+        }
+
+    }
+}
