60ec9c36272f62d82f8d3a76aa7bae706fd0d038
Francisco Gamonal <francisco741@hotmail.com>
2014-08-14 17:04:55 -0500

    Changes register expense

--- public/js/js.js
+++ public/js/js.js
@@ -242,21 +242,11 @@ $(function(){
         newSolicitude();
     }
 
-    //Expense - s
+    //Expense
     
     //Registro de solo números en el campo RUC
     onlyNumber("#ruc");
 
-
-    // $("#ruc").keypress(function(e){
-    //     if(e.which != 8 && e.which != 0 && (e.which <48 || e.which >57))
-    //     {
-    //         return false;
-    //     }
-    // });
-
-    // onlyNumber(ruc);
-    
     //Búsqueda de Razón Social en la SUNAT una vez introducido el RUC
     $("#ruc").on("focusout",function(){
         var ruc        = $(this).val();
@@ -309,54 +299,69 @@ $(function(){
     });
     
     //Removiendo las clases de errores
-    $("#ruc").on("focus",function(){
+    $("input").on("focus",function(){
         $(this).removeClass("error-incomplete");
         $(this).attr("placeholder",'');
-        $(this).val('');
     });
 
-    $("#number_voucher").on("focus",function(){
-        $(this).removeClass("error-incomplete");
-        $(this).attr("placeholder",'');
-        $(this).val('');
-    });
+    
 
-    $("#date").on("focus",function(){
-        $(this).removeClass("error-incomplete");
-        $(this).attr("placeholder",'');
-        $(this).val('');
-    });
+    function validateRuc(ruc)
+    {
+        var rows = $(".total").parent();
+        var ruc_detail = [];
+        $.each(rows,function(index){
+            ruc_detail[index] = $(this).find(".ruc").html();
+        });
+        var index = ruc_detail.indexOf(ruc);
+        if(index>=0)
+            return false;
+        else
+            return true;
+    }
 
-    $("#total").on("focus",function(){
-        $(this).removeClass("error-incomplete");
-        $(this).attr("placeholder",'');
-        $(this).val('');
-    });
+    function validateVoucher(ruc,number_voucher)
+    {
+        var rows = $(".total").parent();
+        var number_voucher_detail = [];
+        $.each(rows,function(index){
+            if(ruc === $(this).find(".ruc").html())
+                number_voucher_detail[index] = $(this).find(".number_voucher").html();
+        });
+        var index = number_voucher_detail.indexOf(number_voucher);
+        if(index>=0)
+            return false;
+        else
+            return true;
+    }
 
-    var balance;
+    //Validación del botón registrar gasto
+    var rows;
     $("#save-expense").on("click",function(){
         var type_voucher   = $("#type_voucher").val();
         var ruc            = $("#ruc").val();
         var razon          = $("#razon").text();
         var razon_hide     = $("#razon").val();
+        var razon_edit     = $("#razon").attr("data-edit");
         var number_voucher = $("#number_voucher").val();
         var date           = $("#date").val();
         var total          = $("#total").val();
-        var error          = 0;
         var deposit        = $("#deposit").val();
-        
+        var error          = 0;
+        var btn_save       = $(this).html();
+
         if(!ruc)
         {
             $("#ruc").attr("placeholder","No se ha ingresado el RUC.");
             $("#ruc").addClass("error-incomplete");
             error = 1;
         }
-        if(razon_hide == 0)
+        if(razon_hide == 0 && razon_edit == 0)
         {
             $("#razon").html("El ruc ingresado no contiene 11 dígitos.");
             error = 1;
         }
-        else if(razon_hide == 1)
+        else if(razon_hide == 1 && razon_edit == 0)
         {
             $("#razon").removeClass("error-incomplete");
             error = 1;
@@ -380,11 +385,19 @@ $(function(){
             error = 1;
         }
 
-        deposit = parseInt(deposit.substring(2,deposit.length));
-        balance = deposit - total;
-        console.log(balance);
+        deposit = parseInt(deposit.substring(2,deposit.length),10);
+        rows = $(".total").parent();
 
+        var balance  = 0;
+        var tot_rows = 0;
+        if(rows)
+        {
+            $.each(rows,function(){
+                tot_rows += parseInt($(this).find(".total").html(),10);
+            });
+        }
 
+        console.log("deposito:"+deposit);
 
         if(error!=0)
         {
@@ -392,31 +405,112 @@ $(function(){
         }
         else
         {
-            var row = "<tr>";
-                row+= "<th class='type_voucher'>"+type_voucher+"</th>";
-                row+= "<th class='ruc'>"+ruc+"</th>";
-                row+= "<th class='razon'>"+razon+"</th>";
-                row+= "<th class='number_voucher'>"+number_voucher+"</th>";
-                row+= "<th class='date'>"+date+"</th>";
-                row+= "<th class='total'>"+total+"</th>";
-                row+= "<th><a class='edit-expense' href='#'><span class='glyphicon glyphicon-pencil'></span></a></th>";
-                row+= "<th><a class='delete-expense' href='#'><span class='glyphicon glyphicon-remove'></span></a></th>";
-                row+= "</tr>";
-            $("#table tbody").append(row);
+            validateRuc(ruc);
+            if(validateRuc(ruc) === true)
+            {
+                balance = deposit - tot_rows - parseInt(total,10);
+                console.log(balance);
+                if(balance<0)
+                {
+                    alert("El monto ingresado supera el monto depositado");
+                    return;
+                }
+                $("#balance").val(balance);
+                var row = "<tr>";
+                    row+= "<th class='type_voucher'>"+type_voucher+"</th>";
+                    row+= "<th class='ruc'>"+ruc+"</th>";
+                    row+= "<th class='razon'>"+razon+"</th>";
+                    row+= "<th class='number_voucher'>"+number_voucher+"</th>";
+                    row+= "<th class='date'>"+date+"</th>";
+                    row+= "<th class='total'>"+total+"</th>";
+                    row+= "<th><a class='edit-expense' href='#'><span class='glyphicon glyphicon-pencil'></span></a></th>";
+                    row+= "<th><a class='delete-expense' href='#'><span class='glyphicon glyphicon-remove'></span></a></th>";
+                    row+= "</tr>";
+                $("#table tbody").append(row);
+            }
+            else
+            {
+                if(validateVoucher(ruc,number_voucher) === true)
+                {
+                    balance = deposit - tot_rows - parseInt(total,10);
+                    console.log(balance);
+                    if(balance<0)
+                    {
+                        alert("El monto ingresado supera el monto depositado");
+                        return;
+                    }
+                    $("#balance").val(balance);
+                    var row = "<tr>";
+                        row+= "<th class='type_voucher'>"+type_voucher+"</th>";
+                        row+= "<th class='ruc'>"+ruc+"</th>";
+                        row+= "<th class='razon'>"+razon+"</th>";
+                        row+= "<th class='number_voucher'>"+number_voucher+"</th>";
+                        row+= "<th class='date'>"+date+"</th>";
+                        row+= "<th class='total'>"+total+"</th>";
+                        row+= "<th><a class='edit-expense' href='#'><span class='glyphicon glyphicon-pencil'></span></a></th>";
+                        row+= "<th><a class='delete-expense' href='#'><span class='glyphicon glyphicon-remove'></span></a></th>";
+                        row+= "</tr>";
+                    $("#table tbody").append(row);
+                }
+                else
+                {
+                    if(btn_save === 'Registrar')
+                    {
+                        alert("El documento ya se encuentra registrado.");
+                    }
+                    else
+                    {
+                        $.each(rows,function(index){
+                            if($(this).find(".number_voucher").html()===number_voucher)
+                            {
+                                $(".date:eq("+index+")").html(date);
+                                $(".total:eq("+index+")").html(total);
+                                console.log("total filas"+tot_rows);
+                                console.log(parseInt($(".total:eq("+index+")").html(),10));
+                                balance = deposit - tot_rows + parseInt($(".total:eq("+index+")").html(),10) - total;
+                            }
+                        });
+
+                        console.log(balance);
+
+                        // if(total>balance)
+                        // {
+                        //     alert("El monto ingresado supera el monto depositado");
+                        //     return;
+                        // }
+                        // else
+                        // {
+                            
+                        //     balance = deposito - balance;
+                        //     $("#balance").html(balance);
+                        // }
+                    }
+                }
+            }
         }
-
-
-
-
     });
     
     $(document).on("click","#table .delete-expense",function(e){
         e.preventDefault();
-        var row = $(this).parent().parent();
+        var tot_del  = $(this).parent().parent().find(".total").html();
+        var row_del  = $(this).parent().parent();
+        var deposit  = $("#deposit").val();
+        var tot_rows = 0;
+        var balance;
+        deposit = parseInt(deposit.substring(2,deposit.length),10);
+        tot_del = parseInt(tot_del,10);
+        
+        rows = $(".total").parent();
+        $.each(rows,function(){
+            tot_rows += parseInt($(this).find(".total").html(),10);
+        });
+    
         bootbox.confirm("¿Esta seguro que desea eliminar el gasto?", function(result) {
             if(result)
             {
-                row.remove();
+                balance = deposit - tot_rows + tot_del;
+                $("#balance").val(balance);
+                row_del.remove();
             }
         });
     });
@@ -426,6 +520,17 @@ $(function(){
         $("#table tbody tr").removeClass("select-row");
         $("#type_voucher option").attr("selected",false);
         $("#save-expense ").html("Actualizar");
+        $("#razon").attr("data-edit",1);
+
+        var deposit  = $("#deposit").val();
+        deposit = parseInt(deposit.substring(2,deposit.length),10);
+
+        var tot=0;
+        var rows = $(".total").parent();
+        $.each(rows,function(){
+            tot += parseInt($(this).find(".total").html(),10);
+        });
+
         var row = $(this).parent().parent();
         row.toggleClass("select-row");
         $.each(row,function(){
@@ -435,10 +540,13 @@ $(function(){
             var number_voucher_edit = $(this).find(".number_voucher").html();
             var date_edit           = $(this).find(".date_movement").html();
             var total_edit          = $(this).find(".total").html();
+            var balance = deposit - tot + parseInt(total_edit,10);
+            $("#balance").val(balance);
             $("#type_voucher option").filter(function(){
                 return $(this).text() == type_voucher_edit;
             }).attr('selected', true);
-            $("#ruc").val(ruc_edit);
+            $("#type_voucher").attr("readonly",true);
+            $("#ruc").val(ruc_edit).attr("readonly",true);
             $("#razon").html(razon_edit).css("color","#5c5c5c");
             $("#number_voucher").val(number_voucher_edit);
             $("#date").val(date_edit);
