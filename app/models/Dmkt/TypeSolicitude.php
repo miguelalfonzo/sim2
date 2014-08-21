609bfaf28edacc65376ee32b551dc3a7913607bc
cesar <cesarhm1687@gmail.com>
2014-08-21 14:44:59 -0500

    created model TypeSolicitude

---
+++ app/models/Dmkt/TypeSolicitude.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Developer
+ * Date: 21/08/14
+ * Time: 12:27 PM
+ */
+
+
+namespace Dmkt;
+use \Eloquent;
+class TypeSolicitude extends Eloquent{
+
+    protected $table = 'DMKT_RG_TIPO_SOLICITUD';
+    protected $primaryKey = 'IDTIPOSOLICITUD';
+
+
+}
