4f553ed8766be69220ef10a8631a608ee75e8e74
idkc <manueltemple@gmail.com>
2015-02-26 12:52:09 -0500

    set RM|SUP desde:HOY-7d hasta:HOY  CONT desde:HOY-2m hasta:HOY

--- app/views/Dmkt/Rm/view_solicitude.blade.php
+++ app/views/Dmkt/Rm/view_solicitude.blade.php
@@ -121,7 +121,7 @@
 
         <div class="input-group date">
             <span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>
-            <input id="date" type="text" class="form-control" maxlength="10" disabled placeholder=""
+            <input id="date" type="text" class="form-control" maxlength="10" readonly placeholder=""
                    value="{{ date_format(date_create($solicitude->fecha_entrega), 'd/m/Y' )}}">
         </div>
 
@@ -135,7 +135,7 @@
     <div class="col-sm-12 col-md-12 col-lg-12">
         <div class="input-group date">
             <span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>
-            <input id="date" type="text" class="form-control" maxlength="10" disabled placeholder=""
+            <input id="date" type="text" class="form-control" maxlength="10" readonly placeholder=""
                    value="{{ date_format(date_create($solicitude->created_at), 'd/m/Y' )}}">
 
         </div>
