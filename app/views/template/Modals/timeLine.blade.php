dc6088bb0b7570b94547e84833230481019bc960
Francisco Gamonal <francisco741@hotmail.com>
2015-04-29 17:02:55 -0500

    timeLine modal

---
+++ app/views/template/Modals/timeLine.blade.php
@@ -0,0 +1,71 @@
+<link rel="stylesheet" type="text/css" href="{{asset('css/timeline.css')}}">
+<!-- Large modal -->
+<div class="modal fade timeLineModal" tabindex="-1" role="dialog" aria-labelledby="mytimeLineModalLabel" aria-hidden="true">
+ 	<div class="modal-dialog modal-lg">
+	    <div class="modal-content">
+	    	<div class="container-fluid">
+	    		<div class="thumbnail">
+					<div class="stage-container">
+						<div class="stage success col-md-2 col-sm-2">
+							<div class="stage-header stage-success"></div>
+							<div class="stage-content">
+								<h3 class="stage-title">Registro de Solicitud</h3>
+								<div class="stage-info">
+									Información Registrada por el Representante Médico
+								</div> 
+							</div>
+						</div>
+						<div class="stage pending col-md-2 col-sm-2">
+							<div class="stage-header stage-pending"></div>
+							<div class="stage-content">
+								<h3 class="stage-title">Evaluación de Solicitud</h3>
+								<div class="stage-info">
+									Evaluación de la Solicitud por el Supervisor
+								</div>
+							</div>
+						</div>
+						<div class="stage col-md-2 col-sm-2">
+							<div class="stage-header"></div>
+							<div class="stage-content">
+								<h3 class="stage-title">Evaluación de la Solicitud</h3>
+								<div class="stage-info">
+									Evaluación de la Solicitud por el Gerente de Producto
+								</div>
+							</div>
+						</div>
+						<div class="stage col-md-2 col-sm-2">
+							<div class="stage-header"></div>
+							<div class="stage-content">
+								<h3 class="stage-title">Aprobación de la Solicitud</h3>
+								<div class="stage-info">
+									Aprobación de la Solicitud por el Gerente Comercial
+								</div> 
+							</div>
+						</div>
+						<div class="stage col-md-2 col-sm-2">
+							<div class="stage-header"></div>
+							<div class="stage-content">
+								<h3 class="stage-title">Depósito de la Solicitud</h3>
+								<div class="stage-info">
+									Solicitud depositada por Tesorería
+								</div> 
+							</div>
+						</div>
+						<div class="stage col-md-2 col-sm-2">
+							<div class="stage-header"></div>
+							<div class="stage-content">
+								<h3 class="stage-title">Registro de Gastos</h3>
+								<div class="stage-info">
+									Aprobación de la Solicitud por el Gerente Comercial
+								</div> 
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+	    </div>
+	    <div class="modal-footer">
+	    	<button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+	    </div>
+	</div>
+</div>
