401d166c24d9acf0f64f916f1bb9007243d1b1bf
idkc <manueltemple@gmail.com>
2015-07-01 13:01:20 -0500

    fix solicitude

--- app/views/Dmkt/Solicitud/Detail/products.blade.php
+++ app/views/Dmkt/Solicitud/Detail/products.blade.php
@@ -1,41 +1,36 @@
 <div class="form-group col-xs-12 col-sm-12 col-md-6 col-lg-6">
-    <div style="padding: 0 15px">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title">Productos</h3>
-            </div>
-            <div class="panel-body">
-                @foreach( $solicitud->products as $product )
-                    <div class="form-group col-xs-12 col-sm-12 col-md-12 col-lg-12"  style="padding: 0">
-                        <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-                            <input type="text" class="form-control input-md"
-                            value="{{{ is_null( $product->marca ) ? '' : $product->marca->descripcion}}}" readonly>
+
+    <div class="panel panel-default">
+        <!-- Default panel contents -->
+        <div class="panel-heading">Productos</div>
+        <!-- List group -->
+        <ul class="list-group">
+            @foreach( $solicitud->products as $product )
+            <li class="list-group-item">
+                
+                
+                    @if($politicStatus)
+                        <div class="input-group">
+                            <span class="input-group-addon" style="width: 70%;">{{{ is_null( $product->marca ) ? '' : $product->marca->descripcion}}}</span>
+                            <span class="input-group-addon amount_families">{{ $detalle->typemoney->simbolo }}</span>
+                            <input name="monto_producto[]" type="text" class="form-control text-right" value="{{ isset( $product->monto_asignado ) ? $product->monto_asignado : 
+                        round( $detalle->monto_actual / count( $solicitud->products ) , 2 ) }}">
                         </div>
-                        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4" style="padding: 0">
-                            <div class="input-group">
-                                <span class="input-group-addon">
-                                    {{ $detalle->typemoney->simbolo }}
-                                </span>
-                                @if ( $politicStatus )
-                                    <input name="monto_producto[]" type="text"
-                                    class="form-control input-md amount_families"
-                                    value="{{ isset( $product->monto_asignado ) ? $product->monto_asignado : 
-                                    round( $detalle->monto_actual / count( $solicitud->products ) , 2 ) }}">
-                                @else
-                                    <input disabled name="monto_producto[]" type="text"
-                                    class="form-control input-md amount_families"
-                                    value="{{ isset( $product->monto_asignado ) ? $product->monto_asignado :
-                                    round( $detalle->monto_actual / count( $solicitud->products ) , 2 ) }}">
-                                @endif
-                            </div>
-                            <input type="hidden" name="producto[]" value="{{ $product->id }}">
-                        </div>
-                    </div>
-                @endforeach
-                <div class="form-group col-sm-12 col-md-12 col-lg-12">
-                    <span id="amount_error_families"></span>
-                </div>
-            </div>
-        </div>
+                    @else
+                        {{{ is_null( $product->marca ) ? '' : $product->marca->descripcion}}}
+                        <span class="badge">{{ $detalle->typemoney->simbolo }} 
+                        {{ isset( $product->monto_asignado ) ? $product->monto_asignado :
+                        round( $detalle->monto_actual / count( $solicitud->products ) , 2 ) }}
+                        </span>
+                    @endif
+                <input type="hidden" name="producto[]" value="{{ $product->id }}">
+            </li>
+            @endforeach
+        </ul>
+    </div>
+    
+    <div class="form-group col-sm-12 col-md-12 col-lg-12">
+        <span id="amount_error_families"></span>
     </div>
 </div>
+        
