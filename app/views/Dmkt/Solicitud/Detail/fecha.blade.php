7013f2ecf9f49146ad097a431c73f6dc7ec70441
Jhonattan Ortiz <horuakti@outlook.com>
2015-07-10 11:43:56 -0500

    rename fecha de entrega to fecha de entrega de dinero

--- app/views/Dmkt/Solicitud/Detail/fecha.blade.php
+++ app/views/Dmkt/Solicitud/Detail/fecha.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group col-xs-12 col-sm-6 col-md-4 col-lg-4">
-    <label class="control-label">Fecha de Creacion / Fecha de Entrega</label>
+    <label class="control-label">Fecha de Creacion / Fecha de Entrega de Dinero</label>
     <div class="input-group date col-xs-12 col-sm-6 col-md-6 col-lg-6 pull-left">
         <span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>
         <input type="text" class="form-control" maxlength="10" disabled value="{{ date_format( date_create( $solicitud->created_at ) , 'd/m/Y' ) }}">
