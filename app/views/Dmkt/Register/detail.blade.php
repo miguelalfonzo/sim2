48288ad6e1c9fc05a40bdee38553d3e2bc11e1c0
Jhonattan Ortiz <horuakti@outlook.com>
2015-09-04 11:57:51 -0500

    description field add comment

--- app/views/Dmkt/Register/detail.blade.php
+++ app/views/Dmkt/Register/detail.blade.php
@@ -100,7 +100,7 @@
         <div class="form-group">
             <label class="control-label" for="descripcion">Descripcion de la Solicitud</label>
             <div>
-                <textarea class="form-control" name="descripcion" maxlength="200">{{ isset( $solicitud->descripcion ) ? $solicitud->descripcion : null }}</textarea>
+                <textarea class="form-control" name="descripcion" maxlength="200" placeholder="(maximo 200 caracteres)">{{ isset( $solicitud->descripcion ) ? $solicitud->descripcion : null }}</textarea>
             </div>
         </div>
     </div>
