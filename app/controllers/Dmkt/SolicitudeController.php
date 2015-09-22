bb930e32e2648ed3d1810d3872430fb6b8bb9bbc
mmanriquedev <mmanrique.dev@gmail.com>
2015-09-22 16:53:37 -0500

    test 7

--- app/controllers/Dmkt/SolicitudeController.php
+++ app/controllers/Dmkt/SolicitudeController.php
@@ -141,31 +141,25 @@ class SolicitudeController extends BaseController
 
     public function addFamilyFundSolicitud()
     {
-//        try
-//        {
-            $solicitudProduct = SolicitudProduct::where('id_solicitud', 9)
-                ->where('id_producto', 12)
+        try
+        {
+            $inputs =   Input::all();
+
+            $solicitudId =  $inputs['solicitud_id'];
+            $productoId=  $inputs['family_id'];
+            $solicitudProduct = SolicitudProduct::where('id_solicitud', $solicitudId)
+                ->where('id_producto', $productoId)
                 ->first();
+            if (is_null($solicitudProduct->id))
+                return $this->setRpta( true );
+            else
+                return $this->setRpta( false );
 
-            dd($solicitudProduct);
-
-//            $inputs =   Input::all();
-//
-//            $solicitudId =  $inputs['solicitud_id'];
-//            $productoId=  $inputs['family_id'];
-//            $solicitudProduct = SolicitudProduct::where('id_solicitud', $solicitudId)
-//                ->where('id_producto', $productoId)
-//                ->first();
-//            if (is_null($solicitudProduct))
-//                return $this->setRpta( true );
-//            else
-//                return $this->setRpta( false );
-//
-//        }
-//        catch( Exception $e )
-//        {
-//            return $this->internalException( $e , __FUNCTION__ );
-//        }
+        }
+        catch( Exception $e )
+        {
+            return $this->internalException( $e , __FUNCTION__ );
+        }
     }
 
     public function viewSolicitude($token)
