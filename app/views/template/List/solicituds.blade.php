e05f4d6140a2e90a0cb2bf0dad6260c3e3e2e8e5
Jhonattan Ortiz <horuakti@outlook.com>
2015-10-06 09:55:17 -0500

    rev fecha entrega for institutional solicitud

--- app/views/template/List/solicituds.blade.php
+++ app/views/template/List/solicituds.blade.php
@@ -68,25 +68,28 @@
                     @endif
                 </td>
                  @if( in_array( Auth::user()->type , array( TESORERIA, CONT ) ))
-                    
-                    <?php $now =  Carbon\Carbon::now();
-                     $fecha_entrega = Carbon\Carbon::createFromFormat( 'd/m/Y' , $solicitud->detalle->fecha_entrega );
-                     $fecha_deposito = Carbon\Carbon::createFromFormat( 'd/m/Y' , $solicitud->detalle->fecha_entrega )->format( 'Y-m-d' ); ?>
-
-                    @if($now >  $fecha_entrega)
-                    <td class="text-center alert-danger">
-                     <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>  
-                     {{ $fecha_deposito}}
-                     </td>  
-                    @elseif ($now->diffInDays($fecha_entrega) <= 1 ) 
-                     <td class="text-center alert-warning">
-                        <span class="glyphicon glyphicon-warning-sign" aria-hidden="true"></span>
-                        <strong>{{ $fecha_deposito}}</strong>
-                    </td>  
+                    <?php $now =  Carbon\Carbon::now(); ?>
+                    @if ( $solicitud->idtiposolicitud == SOL_INST )
+                        <?php $fecha_entrega = Carbon\Carbon::createFromFormat( 'Ym' , $solicitud->detalle->fecha_entrega );
+                        $fecha_deposito = Carbon\Carbon::createFromFormat( 'Ym' , $solicitud->detalle->fecha_entrega )->firstOfMonth()->format( 'Y-m-d' ); ?>
                     @else
-                     <td class="text-center">
-                    {{ $fecha_deposito}}
-                    </td>  
+                        <?php $fecha_entrega = Carbon\Carbon::createFromFormat( 'd/m/Y' , $solicitud->detalle->fecha_entrega );
+                        $fecha_deposito = Carbon\Carbon::createFromFormat( 'd/m/Y' , $solicitud->detalle->fecha_entrega )->format( 'Y-m-d' ); ?>
+                    @endif
+                    @if( $now >  $fecha_entrega )
+                        <td class="text-center alert-danger">
+                            <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>  
+                            {{ $fecha_deposito}}
+                         </td>  
+                    @elseif ($now->diffInDays($fecha_entrega) <= 1 ) 
+                         <td class="text-center alert-warning">
+                            <span class="glyphicon glyphicon-warning-sign" aria-hidden="true"></span>
+                            <strong>{{ $fecha_deposito}}</strong>
+                        </td>  
+                    @else
+                         <td class="text-center">
+                            {{ $fecha_deposito }}
+                        </td>  
                     @endif
                 @else
                 <td class="text-center">
