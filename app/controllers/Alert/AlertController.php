84d69b95d5691b8782b55878bdc1bfc578899504
Jhonattan Ortiz <horuakti@outlook.com>
2016-05-06 19:58:59 -0500

    enable client alert

--- app/controllers/Alert/AlertController.php
+++ app/controllers/Alert/AlertController.php
@@ -6,10 +6,12 @@ use \BaseController;
 use \Exception;
 use \Carbon\Carbon;
 use \Dmkt\Solicitud;
+use \Dmkt\SolicitudClient;
 use \Auth;
 use \Illuminate\Database\Eloquent\Collection;
 use \Parameter\Parameter;
 use \View;
+use \DB;
 
 class AlertController extends BaseController
 {
@@ -24,31 +26,14 @@ class AlertController extends BaseController
 		return array('status' => 'OK', 'alerts' => $this->alertConsole2());
 	}
 
-	private function intersectRecords( $rs1 , $rs2 )
-    {
-    	$intersect = new Collection;
-    	foreach( $rs1 as $r1 )
-    	{
-    		foreach( $rs2 as $r2 )
-    		{
-    			if ( $r2->id_cliente == $r1->id_cliente && $r2->id_tipo_cliente == $r1->id_tipo_cliente )
-    			{
-    				$intersect->add( $r2 );
-    				break;
-    			}
-    		}
-    	}
-    	return $intersect;
-    }
-
     public function alertConsole2()
     {
     	$result = array();
     	$result['alert'] = array();
     	
-    	//$clientAlert = $this->clientAlert2();
-    	/*if($clientAlert[ 'msg' ] != "" )
-    		$result['alert'][] = $clientAlert;*/
+    	$clientAlert = $this->clientAlert2();
+    	if($clientAlert[ 'msg' ] != "" )
+    		$result['alert'][] = $clientAlert;
     	
     	$expenseAlert = $this->expenseAlert2();
     	if($expenseAlert[ 'msg' ] != "")
@@ -60,216 +45,74 @@ class AlertController extends BaseController
     	
     	return $result['alert'];
     }
-    public function alertConsole()
-    {
-    	$result = array();
-    	$result['alert'] = array();
-    	
-    	/*$clientAlert = $this->clientAlert();
-    	if($clientAlert[ 'msg' ] != "" )
-    		$result['alert'][] = $clientAlert;*/
-    	
-    	$expenseAlert = $this->expenseAlert();
-    	if($expenseAlert[ 'msg' ] != "")
-    		$result['alert'][] = $expenseAlert;
-
-    	$timeAlert = $this->compareTime();
-    	if($timeAlert[ 'msg' ] != "")
-    		$result['alert'][] = $timeAlert;
-
-    	return $result['alert'];
-    }
 
     public function clientalert2()
     {
-    	$msg = '';
-    	$tipo_cliente_requerido = array( MEDICO , INSTITUCION );
-    	$tiempo = Parameter::find( ALERTA_INSTITUCION_CLIENTE );
-		$solicituds = Solicitud::all();
-		foreach ( $solicituds as $key => $solicitud )
-		{
-			$clients = $solicitud->clients;
-			$solicitud_tipo_cliente = array_unique( $clients->lists( 'id_tipo_cliente') );
-			if ( count( array_intersect( $solicitud_tipo_cliente, $tipo_cliente_requerido ) ) <= 1 )
-				unset( $solicituds[ $key ] );
-		}
-		$clientList         = array();
-		$compare_second_id  = array();
-		$compare_initial_id = array();
-		$clienteArray       = array();
-		$result             = array();
-		foreach( $solicituds as $solicitud_inicial )
-		{
-			foreach ( $solicituds as $solicitud_secundaria )
-			{
-				if ( $solicitud_secundaria->id != $solicitud_inicial->id && ( ! in_array( $solicitud_secundaria->id , $compare_initial_id ) ) )
-				foreach( $solicituds as $solicitud_final )
-				{
-					if( $solicitud_final->id != $solicitud_secundaria->id && 
-						$solicitud_final->id != $solicitud_inicial->id && 
-						( ! in_array( $solicitud_final->id , $compare_second_id ) ) )
-					{
-						$clients_inicial = $solicitud_inicial->clients()->select( 'id_cliente' , 'id_tipo_cliente' )->get();
-						$clients_secundaria = $solicitud_secundaria->clients()->select( 'id_cliente' , 'id_tipo_cliente' )->get();
-						$clients_final = $solicitud_final->clients()->select( 'id_cliente' , 'id_tipo_cliente' )->get();
-						//echo $solicitud_inicial->id . '|' . $solicitud_secundaria->id . '|' . $solicitud_final->id . '<br>';
-						$intersect_client = $this->intersectRecords( $clients_inicial , $clients_secundaria );
-						$intersect_client = $this->intersectRecords( $intersect_client , $clients_final );
-						$solicitud_tipo_cliente = array_unique( $intersect_client->lists( 'id_tipo_cliente' ) );
-						if ( count( array_intersect( $solicitud_tipo_cliente, $tipo_cliente_requerido ) ) >= 2 )
-						{
-							$cliente = '( ';
-							foreach ( $intersect_client as $client_inicial )
-							{
-								$clienteArray[] = $client_inicial->{$client_inicial->clientType->relacion}->full_name;
-								$cliente .= $client_inicial->{$client_inicial->clientType->relacion}->full_name .  ' , ' ;
-							} 
-							$cliente = rtrim( $cliente , ', ' );
-							$cliente .= ' ).';
-							$msg .= 'Las solicitudes ' . $solicitud_inicial->id . ' , ' . $solicitud_secundaria->id . ' , ' . $solicitud_final->id . ' ' . $tiempo->mensaje . ' ' . $cliente;
-							$result[]=array(
-								'cliente'    => $clienteArray,
-								'solicitude' => array($solicitud_inicial->id, $solicitud_secundaria->id, $solicitud_final->id),
-								'msg'     => $tiempo->mensaje
-							);
-							$clienteArray = array();
-						}
-					}
+    	$user = Auth::user();
+    	if( in_array( $user->type , [ REP_MED , SUP , GER_PROD , GER_PROM , GER_COM , CONT ] ) )
+    	{
+	    	$add;
+	    	$msg;
+	    	$result = [];				
+	    	$clienteArray;
+	    	$solicitudArray;
+	    	$data = DB::table( 'VALIDACION_CLIENTE' )->get();
+	    	$tiempo = Parameter::find( ALERTA_INSTITUCION_CLIENTE );
+	    	foreach( $data as $register )
+	    	{
+	    		$msg = 'Las solicitudes ' . $register->ids_solicitud  . ' ' . $tiempo->mensaje ;
+	 			$add = false;
+	    		$ids_solicitud = explode( ',' , $register->ids_solicitud );
+	    		foreach( $ids_solicitud as $id_solicitud )
+	    		{
+	    			$solicitud = Solicitud::find( $id_solicitud );
+	    			if( $user->type === REP_MED )
+	    			{
+	    				if( $solicitud->id_user_assign == $user->id )
+	    				{
+	    					$add = true;
+	    				}
+	    			}
+	    			elseif( in_array( $user->type , [ SUP , GER_PROD , GER_PROM ] , 1 ) )
+	    			{
+	    				$ids = $solicitud->gerente->lists( 'id_gerprod' );
+	    				if( in_array( $user->id , $ids ) )
+	    				{
+	    					$add = true;
+	    				}
+	    			}
+	    			elseif( in_array( $user->type , [ GER_COM , CONT ] , 1 ) )
+	    			{
+	    				$add = true;
+	    			}
+	    		}
+	    		if( $add )
+	    		{
+	    			$clients = explode( ',' , $register->cliente );
+	    			foreach( $clients as $client )
+	    			{
+	    				$client_data      = explode( '|' , $client );
+	    				$solicitudCliente = SolicitudClient::where( 'id_cliente' , $client_data[ 0 ] )->where( 'id_tipo_cliente' , $client_data[ 1 ] )->first();
+	    				$clientName     = $solicitudCliente->{$solicitudCliente->clientType->relacion}->full_name;
+	    				$clienteArray[] = $clientName;
+	    				$cliente = '( ' . $clientName . ' , ';
+					} 
+					$cliente = rtrim( $cliente , ', ' );
+					$cliente .= ' ).';
+					$msg .= ' ' . $cliente;
+	    			$solicitudArray = $ids_solicitud;
+	    			$result[]= 
+	    			[
+						'cliente'    => $clienteArray,
+						'solicitude' => $ids_solicitud,
+						'msg'        => $tiempo->mensaje
+					];
+					$clienteArray = [];
 				}
-				$compare_second_id[] = $solicitud_secundaria->id;
-			}
-			$compare_second_id = array();
-			$compare_initial_id[] = $solicitud_inicial->id;
-		}    	
-		return array( 'type' => 'warning' , 'msg' => $msg, 'data' => $result, 'typeData' => 'clientAlert');
+	    	}
+	    }
+	    return array( 'type' => 'warning' , 'msg' => $msg , 'data' => $result, 'typeData' => 'clientAlert');
 	}
-
-    public function clientalert222()
-    {
-    	$result = array();
-    	$msg = '';
-    	$tipo_cliente_requerido = array( MEDICO , INSTITUCION );
-    	$tiempo = Parameter::find( ALERTA_INSTITUCION_CLIENTE );
-		$solicituds = Solicitud::all();
-		foreach ( $solicituds as $key => $solicitud )
-		{
-			$clients = $solicitud->clients;
-			$solicitud_tipo_cliente = array_unique( $clients->lists( 'id_tipo_cliente') );
-			if ( count( array_intersect( $solicitud_tipo_cliente, $tipo_cliente_requerido ) ) <= 1 )
-				unset( $solicituds[ $key ] );
-		}
-		$clientList = array();
-		$solicituds_compare_id = array();
-		foreach( $solicituds as $solicitud_inicial )
-		{
-			$clients_inicial = $solicitud_inicial->clients()->select( 'id_cliente' , 'id_tipo_cliente' )->get();
-			foreach ( $solicituds as $solicitud_secundaria )
-			{
-				if ( $solicitud_inicial->id != $solicitud_secundaria->id && ! in_array( $solicitud_secundaria->id , $solicituds_compare_id ) )
-				{
-					$clients_secundaria = $solicitud_secundaria->clients()->select( 'id_cliente' , 'id_tipo_cliente' )->get();
-					$cliente_inicial    = $this->intersectRecords( $clients_inicial , $clients_secundaria );
-					if ( ! $cliente_inicial->isEmpty() )
-					{
-						foreach( $solicituds as $solicitud_final )
-						{
-							if ( $solicitud_inicial->id != $solicitud_final->id && $solicitud_final->id != $solicitud_secundaria->id && ! in_array( $solicitud_final->id , $solicituds_compare_id ) && ( $this->diffCreatedAt( $solicitud_inicial , $solicitud_secundaria , $solicitud_final ) <= $tiempo->valor ) )
-							{
-								$clients_final = $solicitud_final->clients()->select( 'id_cliente' , 'id_tipo_cliente' )->get();
-								$cliente_inicial = $this->intersectRecords( $clients_inicial , $clients_final );
-								if ( ! $cliente_inicial->isEmpty() )
-								{
-									$solicitud_tipo_cliente = array_unique( $cliente_inicial->lists( 'id_tipo_cliente' ) );
-									if ( count( array_intersect( $solicitud_tipo_cliente, $tipo_cliente_requerido ) ) >= 2 )
-									{
-										$clienteArray=array();
-										$cliente = '( ';
-										foreach ( $cliente_inicial as $client_inicial ){
-											$clienteArray[] = $client_inicial->{$client_inicial->clientType->relacion}->full_name;
-											$cliente .= $client_inicial->{$client_inicial->clientType->relacion}->full_name .  ' , ' ;
-
-										} 
-										$cliente = rtrim( $cliente , ', ' );
-										$cliente .= ' ).';
-										$msg .= 'Las solicitudes ' . $solicitud_inicial->id . ' , ' . $solicitud_secundaria->id . ' , ' . $solicitud_final->id . ' ' . $tiempo->mensaje . ' ' . $cliente;
-										$solicituds_compare_id[] = $solicitud_inicial->id;
-										$solicituds_compare_id[] = $solicitud_secundaria->id;
-										$result[]=array(
-												'cliente'    => $clienteArray,
-												'solicitude' => array($solicitud_inicial->id, $solicitud_secundaria->id, $solicitud_final->id),
-												'msg'     => $tiempo->mensaje
-											);
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		return array( 'type' => 'warning' , 'msg' => $msg, 'data' => $result, 'typeData' => 'clientAlert');
-    }
-
-    public function clientalert()
-    {
-    	$msg = '';
-    	$tipo_cliente_requerido = array( MEDICO , INSTITUCION );
-    	$tiempo = Parameter::find( ALERTA_INSTITUCION_CLIENTE );
-		$solicituds = Solicitud::all();
-		foreach ( $solicituds as $key => $solicitud )
-		{
-			$clients = $solicitud->clients;
-			$solicitud_tipo_cliente = array_unique( $clients->lists( 'id_tipo_cliente') );
-			if ( count( array_intersect( $solicitud_tipo_cliente, $tipo_cliente_requerido ) ) <= 1 )
-				unset( $solicituds[ $key ] );
-		}
-		$clientList = array();
-		$solicituds_compare_id = array();
-		foreach( $solicituds as $solicitud_inicial )
-		{
-			$clients_inicial = $solicitud_inicial->clients()->select( 'id_cliente' , 'id_tipo_cliente' )->get();
-			foreach ( $solicituds as $solicitud_secundaria )
-			{
-				if ( $solicitud_inicial->id != $solicitud_secundaria->id && ! in_array( $solicitud_secundaria->id , $solicituds_compare_id ) )
-				{
-					$clients_secundaria = $solicitud_secundaria->clients()->select( 'id_cliente' , 'id_tipo_cliente' )->get();
-					$cliente_inicial = $this->intersectRecords( $clients_inicial , $clients_secundaria );
-					if ( ! $cliente_inicial->isEmpty() )
-					{
-						foreach( $solicituds as $solicitud_final )
-						{
-							if ( $solicitud_inicial->id != $solicitud_final->id && $solicitud_final->id != $solicitud_secundaria->id && ! in_array( $solicitud_final->id , $solicituds_compare_id ) && ( $this->diffCreatedAt( $solicitud_inicial , $solicitud_secundaria , $solicitud_final ) <= $tiempo->valor ) )
-							{
-								$clients_final = $solicitud_final->clients()->select( 'id_cliente' , 'id_tipo_cliente' )->get();
-								$cliente_inicial = $this->intersectRecords( $clients_inicial , $clients_final );
-								if ( ! $cliente_inicial->isEmpty() )
-								{
-									$solicitud_tipo_cliente = array_unique( $cliente_inicial->lists( 'id_tipo_cliente' ) );
-									if ( count( array_intersect( $solicitud_tipo_cliente, $tipo_cliente_requerido ) ) >= 2 )
-									{
-										$cliente = '( ';
-										foreach ( $cliente_inicial as $client_inicial )
-											$cliente .= $client_inicial->{$client_inicial->clientType->relacion}->full_name .  ' , ' ; 
-										$cliente = rtrim( $cliente , ', ' );
-										$cliente .= ' ).';
-										$msg .= 'Las solicitudes ' . $solicitud_inicial->id . ' , ' . $solicitud_secundaria->id . ' , ' . $solicitud_final->id . ' ' . $tiempo->mensaje . ' ' . $cliente;
-										$solicituds_compare_id[] = $solicitud_inicial->id;
-										$solicituds_compare_id[] = $solicitud_secundaria->id;
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		return array( 'type' => 'warning' , 'msg' => $msg );
-    }
-
-    private function solMsg( $record1 , $record2 , $record3 )
-    {
-    	return $record1->id . /*' ' . $record1->created_at .*/ ' , ' . $record2->id . ' ' . /*$record2->created_at . ' ' .*/ $record3->id . ' ' /*. $record3->created_at*/ ; 
-    }
 
     public function expenseAlert2()
 	{
@@ -299,48 +142,12 @@ class AlertController extends BaseController
 		return array( 'type' => 'warning' , 'msg' => $msg, 'typeData' => 'expenseAlert', 'data' => $result );
 	}
 
-	public function expenseAlert()
-	{
-		$solicituds = Solicitud::where( 'id_user_assign' , Auth::user()->id )->where( 'id_estado' , GASTO_HABILITADO )->get();
-		$msg = '';
-		$tiempo = Parameter::find( ALERTA_TIEMPO_ESPERA_POR_DOCUMENTO );
-		foreach ( $solicituds as $solicitud )
-		{
-			$expenseHistory = $solicitud->expenseHistory;
-			$lastExpense = $solicitud->lastExpense;
-			if ( is_null( $lastExpense ) && ! is_null( $expenseHistory ) && $this->timeAlert( $expenseHistory , 'diffInDays' , 'updated_at' ) >= $tiempo->valor )
-				$msg .= 'La solicitud N° ' .  $solicitud->id . $tiempo->mensaje;
-			else if ( ( ! is_null( $lastExpense ) ) && $this->timeAlert( $lastExpense , 'diffInDays' , 'updated_at' ) >= $tiempo->valor )
-				$msg .= 'La solicitud N° ' .  $solicitud->id . ' ' .  $tiempo->mensaje;	
-		}
-		return array( 'type' => 'warning' , 'msg' => $msg );
-	}
 
 	public function timeAlert( $record , $method , $date )
 	{
 		$now = Carbon::now();
 		$updated = new Carbon( $record->$date );
 		return $updated->$method( $now );
-	}
-
-	private function diffCreatedAt( $record1 , $record2 , $record3 )
-	{
-		$date1 = new Carbon( $record1->created_at );
-		$date2 = new Carbon( $record2->created_at );
-		$date3 = new Carbon( $record3->created_at );
-		$min = $date1->min( $date2 , $date3 );
-		$max = $date1->max( $date2 , $date3 );
-		$rpta = $max->diffInDays( $min );
-		return $rpta;
-	}
-
-	public function compareTime( $record , $method )
-	{
-		$tiempo = Parameter::find( ALERTA_TIEMPO_REGISTRO_GASTO );
-		if ( $this->timeAlert( $record , 'diffInDays' , 'created_at' ) >= $tiempo->valor )
-			return array( 'type' => 'warning' , 'msg' => $tiempo->mensaje );
-		else
-			return array();
 	}
 
 	public function compareTime2()
