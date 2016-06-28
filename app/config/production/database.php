39063917da2b9234f26f50a1b277cd2199905029
Jhonattan Ortiz <horuakti@outlook.com>
2016-06-28 18:29:30 -0500

    change database ip

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -33,7 +33,7 @@ return array(
 
 		'oracle' => array(
 			'driver' => 'oracle',
-			'host' => '192.168.1.4',
+			'host' => '10.51.1.4',
 			'port' => '1521',
 			'database' => 'BDBAGO',
 			'username' => 'sim',
