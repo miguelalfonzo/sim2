0978f3bbe80a8ff5aad94c2e78ae41e32cbf5be1
Jhonattan Ortiz <horuakti@outlook.com>
2016-08-17 11:43:20 -0500

    clean special character in seat data for bago system

--- app/models/Seat/Seat.php
+++ app/models/Seat/Seat.php
@@ -70,7 +70,11 @@ class Seat extends Eloquent
             $serie = substr( $cuatroDigitosSerie , strlen( $cuatroDigitosSerie ) - 4 , 4 ) . $systemSeat->cbte_prov;
         }
         $seat->pennrocompro  = $serie;
-        $seat->pennombrepro  = substr( Self::blankspace( $systemSeat->nom_prov ) , 0 , 50 ); // RAZON SOCIAL SOLO PARA DOCUMENTOS | maximo 50 digitos.
+        
+        $razon = substr( Self::blankspace( $systemSeat->nom_prov ) , 0 , 50 ); // RAZON SOCIAL SOLO PARA DOCUMENTOS | maximo 50 digitos.
+        $razon = str_replace( "," , " " , $razon );
+        $razon = str_replace( "'" , " " , $razon );
+        $seat->pennombrepro  = $razon;
         $seat->pencoddocpro  = Self::blankspace( $systemSeat->cod ); // CODIGO del sistema para documentos con igv = "80"
         $seat->pennrodocpro  = Self::blankspace( $systemSeat->ruc );  // RUC SOLO PARA DOCUMENTOS
         $seat->pencanthojas  = Self::blankspace( $systemSeat->tipo_responsable ); //CODIGO DE TIPO DE RESPONSABLE 1 , 2, 4 PARA LA SUNAT DE USUARIOS EFECTOS A OPERACION GRAVADA => 1  , NO GRAVADA => 2  O AMBOS = 4
